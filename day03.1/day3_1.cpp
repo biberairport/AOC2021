dca3f6619d09609b7735b63da76ee8807292c44d
Dirk Ve <79271009+biberairport@users.noreply.github.com>
2021-12-10 00:41:59 +0100

    cleaned

--- day03.1/day3_1.cpp
+++ day03.1/day3_1.cpp
@@ -23,84 +23,83 @@ int main()
     while (myInput >> value) {
         vdata.push_back(value);
 	x++;
- }
+    }
 
 
 
     for(int a=0; a<x; a++){
-	for (int i = 0; i<12; i++){
-	   if(vdata[a][i] == '1'){
-	     ti[i]+=1;
-             ta[i]+=1;
-	   }
-	 }
+	    for (int i = 0; i<12; i++){
+	        if(vdata[a][i] == '1'){
+	            ti[i]+=1;
+                 ta[i]+=1;
+	        }
+	    }
     }
 
 
    int helper = 11;
    for(int b = 0; b < 12; b++)
    {
-     if(ti[b] > x/2){
-       ti[b] = 1;}
+        if(ti[b] > x/2){
+            ti[b] = 1;}
        
-     else
-       ti[b] = 0;
+        else
+            ti[b] = 0;
 
-    res1 += ti[b]*pow(2,helper);
-   helper--;
+        res1 += ti[b]*pow(2,helper);
+        helper--;
    }
 
-  helper = 11;
-   for(int b = 0; b < 12; b++)
-   {
-     if(ta[b] < x/2)
-       ta[b] = 1;
-    else
-       ta[b] = 0;
+    helper = 11;
+    for(int b = 0; b < 12; b++)
+    {
+        if(ta[b] < x/2)
+            ta[b] = 1;
+        else
+            ta[b] = 0;
 
-    res2 += ta[b]*pow(2,helper);
-    helper--;
+        res2 += ta[b]*pow(2,helper);
+        helper--;
    }
 
-int g=0;
-  for(int g; g<x; g++)
-   { 
-     for(int helper=11; helper--; helper >=0)
-{
-       if(ti[g]==1)
-        xdata.push_back(vdata[g]);
-}
-}
-
-
- g=0;
-  for(int g; g<x; g++)
-   { 
-     for(int helper=11; helper--; helper >=0)
-	{
-       	if(ti[g]==0)
-            udata.push_back(vdata[g]);
-	}
+    int g=0;
+    for(int g; g<x; g++)
+    { 
+        for(int helper=11; helper--; helper >=0){
+        if(ti[g]==1)
+            xdata.push_back(vdata[g]);
+        }
     }
 
 
-int res3 = 0;
-int res4 = 0;
+    g=0;
+    for(int g; g<x; g++)
+    { 
+        for(int helper=11; helper--; helper >=0)
+	    {
+       	    if(ti[g]==0)
+                udata.push_back(vdata[g]);
+	    }
+    }
 
-helper = 11;
-   for(int b = 0; b < 12; b++)
-   {
-    res3 += xdata[0][b]*pow(2,helper);
-    helper--;
-   }
 
-helper = 11;
-   for(int b = 0; b < 12; b++)
-   {
-    res4 += udata[0][b]*pow(2,helper);
-    helper--;
-   }
+    int res3 = 0;
+    int res4 = 0;
 
-cout << res3 << endl << res4 << endl << res3*res4 << endl;
-cout << g << endl << xdata[0] << endl << udata[0] << endl;
+    helper = 11;
+    for(int b = 0; b < 12; b++)
+    {
+        res3 += xdata[0][b]*pow(2,helper);
+        helper--;
+    }
+
+    helper = 11;
+    for(int b = 0; b < 12; b++)
+    {
+        res4 += udata[0][b]*pow(2,helper);
+        helper--;
+    }
+
+    cout << res3 << endl << res4 << endl << res3*res4 << endl;
+    cout << g << endl << xdata[0] << endl << udata[0] << endl;
 }
